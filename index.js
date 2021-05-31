74787c89572ef1ef245f534e7410eded252d5a85
Talia Williams <williams.talia101@gmail.com>
2021-05-31 01:02:51 -0500

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Your code here
+function saturdayFun(fun1, fun2){
+    if (fun1 === undefined) (fun2 = 'roller-skate')
+    return 'This Saturday, I want to roller-skate !'; 
+}
+
+function mondayWork(agenda2) {
+    if (agenda1 === undefined) (agenda2 = "go to the office")
+    return 'This Monday, I will go to the office.';
+}
+
+function wrapAdjective(bright, word) {
+    if (bright = '*') (word = 'a hard worker')
+    return 'You are *a hard worker*!';
+}
